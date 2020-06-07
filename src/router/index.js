e0e5f656db67f6c5dc4be8c2df0a15d8bb05c34a
arvin.wang <arvin.wang@harman.com>
2020-06-07 22:09:57 +0800

    format login string and login to table

--- src/router/index.js
+++ src/router/index.js
@@ -38,6 +38,12 @@ export const constantRoutes = [
   },
 
   {
+    path: '/deployList',
+    component: () => import('@/views/deploy/index'),
+    hidden: true
+  },
+
+  {
     path: '/404',
     component: () => import('@/views/404'),
     hidden: true
