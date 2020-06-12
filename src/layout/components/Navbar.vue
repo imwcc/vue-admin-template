ec8ab9ee372b1fc2fe6ce193d42d53f58eca4134
Wang, Arvin <arvin.wang@harman.com>
2020-06-12 15:49:22 +0800

    exit google account
    Signed-off-by: Wang, Arvin <arvin.wang@harman.com>

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -52,6 +52,7 @@ export default {
       this.$store.dispatch('app/toggleSideBar')
     },
     async logout() {
+      this.$gapi.signOut()
       await this.$store.dispatch('user/logout')
       this.$router.push(`/login?redirect=${this.$route.fullPath}`)
     }
